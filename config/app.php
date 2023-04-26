e99c6b9c5180ff0356e8e77b210829756c284631
Hesan Naveed <hesannaveed21@gmail.com>
2023-04-26 14:26:18 +0500

    Excel Installation

--- config/app.php
+++ config/app.php
@@ -168,6 +168,7 @@ return [
         // App\Providers\BroadcastServiceProvider::class,
         App\Providers\EventServiceProvider::class,
         App\Providers\RouteServiceProvider::class,
+        Maatwebsite\Excel\ExcelServiceProvider::class,
     ])->toArray(),
 
     /*
@@ -182,6 +183,7 @@ return [
     */
 
     'aliases' => Facade::defaultAliases()->merge([
+        'Excel' => Maatwebsite\Excel\Facades\Excel::class
         // 'Example' => App\Facades\Example::class,
     ])->toArray(),
 
